0e84c4b849d043a339489cd6310e920167964f0a
Marc-Andre Juritz <mjur@ivu.de>
2026-01-26 13:39:33 +0100

    Feature: Authentication - CORS

--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(helmet());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : '*',
         credentials: true,
     })
 );
